b160f8d7424ccd477ec47d5d3a7136494bc2d19c
ThyaraV <thyara.vintimilla@udla.edu.ec>
2024-08-01 15:57:03 -0500

    Pantalla de inicio Paciente

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import HomeScreenPaciente from './screens/HomeScreenPaciente.jsx'
 const App = () => {
   return (
     <>
     <Header/>
     <main className='py-3'>
       <Container>
-      <h1> Seguimiento</h1>
+      <HomeScreenPaciente/>
       </Container>
     </main>
     <Footer/>
---
+++ frontend/src/screens/HomeScreenPaciente.jsx
@@ -0,0 +1,10 @@
+import {Row, Col} from 'react-bootstrap';
+import React from 'react'
+
+const HomeScreenPaciente = () => {
+  return (
+    <div>HomeScreenPaciente</div>
+  )
+}
+
+export default HomeScreenPaciente
