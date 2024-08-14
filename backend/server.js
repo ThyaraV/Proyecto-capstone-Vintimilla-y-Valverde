f18b70a4a108416e78139c7014e62c480c6b04e0
ThyaraV <thyara.vintimilla@udla.edu.ec>
2024-08-14 16:11:24 -0500

    Conexión con mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+import connectDB from './config/db.js';
 const port=process.env.PORT || 5000;
 
+connectDB();//Connect to MongoDB
 const app =express();
+
 
 app.get('/',(req,res)=>{
     res.send('API is running...')
