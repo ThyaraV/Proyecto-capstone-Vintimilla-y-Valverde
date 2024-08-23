e2497d0c7b378ddb24c7b6515551af3ee29889bd
ThyaraV <thyara.vintimilla@udla.edu.ec>
2024-08-23 11:14:09 -0500

    login p3

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken'
 //import generateToken from "../utils/generateToken.js";
 
 //@desc Auth user and get token
@@ -14,6 +15,15 @@ const authUser=asyncHandler(async(req,res)=>{
     console.log('User found:', user);
     
     if (user && (await user.matchPassword(password))) {
+        const token =jwt.sign({userId:user._id}, process.env.JWT_SECRET,{
+            expiresIn:'30d',
+        });
+        res.cookie('jwt', token,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV !== 'development',
+            sameSite:'strict',
+            maxAge:30*24*60*60*1000
+        })
         //generateToken(res, user._id);
         res.status(200).json({
             _id: user._id,
@@ -65,13 +75,12 @@ const registerUser=asyncHandler(async(req,res)=>{
 //@route GET/api/users
 //@access Private
 const logoutUser=asyncHandler(async(req,res)=>{
-    /*res.cookie('jwt','',{
+    res.cookie('jwt','',{
         httpOnly:true,
         expires:new Date(0),
     });
 
-    res.status(200).json({message:'Logged out succesfully'});*/
-    res.send('Logout user');
+    res.status(200).json({message:'Logged out succesfully'});
 });
 
 
