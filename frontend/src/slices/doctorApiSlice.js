9fe2763f4d9f342b492a16f59e78ff3efb0d23da
ThyaraV <thyara.vintimilla@udla.edu.ec>
2024-11-01 22:05:47 -0500

    Update doctorApiSlice.js

--- frontend/src/slices/doctorApiSlice.js
+++ frontend/src/slices/doctorApiSlice.js
@@ -11,7 +11,7 @@ export const doctorApiSlice = apiSlice.injectEndpoints({
     getDoctorWithPatients: builder.query({
       query: () => "/api/doctors/withPatients",
       providesTags: ["Doctor"],
-      refetchOnMountOrArgChange: true,
+      refetchOnMountOrArgChange: true, // Esto asegura que los datos se actualicen al montar el componente
     }),
     addPatientToDoctor: builder.mutation({
       query: ({ doctorId, patientId }) => ({
@@ -29,6 +29,3 @@ export const {
   useAddPatientToDoctorMutation,
   useGetDoctorWithPatientsQuery,
 } = doctorApiSlice;
-
-// Exportar la acción de invalidación manual
-export const invalidateDoctorWithPatients = doctorApiSlice.util.invalidateTags(["Doctor"]);
